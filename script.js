051ccf21ae71e091fd47eaedc9046f8e304f683e
DIOGENES GOMES SANTOS DA SILVA <133171059+StudioProjetosWeb@users.noreply.github.com>
2023-11-23 20:04:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Aqui vamos adicionar o código para enviar os dados para o servidor
+});
 
